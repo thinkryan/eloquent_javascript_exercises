91bcbf116890681b45a0291a7091493ff7b91551
Ryan Dennler <ryan.beatbox@gmail.com>
2020-10-30 10:55:04 -0600

    Adjust solution answers and a solution #2 for the exercise

--- 1.6/borrowing_a_method.js
+++ 1.6/borrowing_a_method.js
@@ -11,14 +11,22 @@
 
 let map = {one: "one", two: "two", hasOwnProperty: true};
 
-console.log(({}).hasOwnProperty.call(map, "one"));
+/**
+ * Solution #1
+ */
+//console.log(({}).hasOwnProperty.call(map, "one"));
+
+/**
+ * Solution #2
+ */
+console.log(Object.prototype.hasOwnProperty.call(map, "three"));
 
 /**
  Solution Description: You can invoke another Object's hasOwnProperty and call it with this set to the object you'd
  like to call it on. (this)
 
- @todo Look into if this copies the original object into another one with just that value and performs the method call.
- @todo i think another way is to Object.prototype.hasOwnProperty.call(foo, 'bar');
+ Solution #1: Copies the object with the argArray value and performs the method call on a new prototype.
+ Solution #2: Doesn't create a new object.
 
  @reference url: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/hasOwnProperty
  */
